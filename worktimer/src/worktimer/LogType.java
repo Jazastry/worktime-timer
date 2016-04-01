5115f691a1bb58b8b75b43a0d2ca919f6fce5a10
yasen <yasen.lazarov@tiger-technology.com>
2016-04-01 20:26:03 +0300

    Second initial - refactoring.

---
+++ worktimer/src/worktimer/LogType.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package worktimer;
+
+/**
+ *
+ * @author yasen.lazarov
+ */
+public enum LogType {
+    LOGIN("LOGIN"),
+    START("START"),
+    STOP("STOP"),
+    LOGOUT("LOGOUT"),
+    PAUSE("PAUSE");
+
+
+    private final String name;
+
+    LogType(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }    
+}
